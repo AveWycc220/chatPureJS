229d0ef0346ce83369d9db5ea7359f4f057791fe
AveWycc220 <mik16748@gmail.com>
2020-12-27 21:11:48 +0300

    feat (views): add login page

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Boilerplate',
+      title: 'Chat',
       template: path.resolve(__dirname, './src/index.html'),
       filename: 'index.html',
     }),
