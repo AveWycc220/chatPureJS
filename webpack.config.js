614db236f01fbf103a4046dfccd1c37fc1d1ecdf
AveWycc220 <mik16748@gmail.com>
2020-12-27 15:53:00 +0300

    feat (package&webpack): add plugin for scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,41 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: [
     './src/index.js',
+    './scss/style.scss'
   ],
   output: {
     filename: './bundle.js'
   },
   devtool: "source-map",
   module: {
-    rules: [{
+    rules: [
+      {
+        test: /\.(sass|scss)$/,
+        include: path.resolve(__dirname, 'scss'),
+        use: ExtractTextPlugin.extract({
+          use: [{
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+              minimize: true,
+              url: true
+            }
+          },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
+      },
+      {
       test: /\.js$/,
-      include: path.resolve(__dirname, 'src/js'),
+      include: path.resolve(__dirname, 'src'),
       use: {
         loader: 'babel-loader',
         options: {
@@ -21,6 +45,11 @@ module.exports = {
     },
     ]
   },
+
   plugins: [
+    new ExtractTextPlugin({
+      filename: './css/style.bundle.css',
+      allChunks: true,
+    }),
   ]
 };
