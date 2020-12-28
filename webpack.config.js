ece510e1c7e837c1fdf49eda1913f8bc336aa931
AveWycc220 <mik16748@gmail.com>
2020-12-28 17:47:50 +0300

    feat (env): add env vars

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const webpack = require('webpack')
+const Dotenv = require('dotenv-webpack')
 
 
 module.exports = {
@@ -58,5 +59,9 @@ module.exports = {
     }),
     new CleanWebpackPlugin(),
     new webpack.HotModuleReplacementPlugin(),
+    new Dotenv({
+      path: './.env',
+      safe: true
+    })
   ],
 }
