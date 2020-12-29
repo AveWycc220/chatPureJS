67ca114d6aceafb9c17e7f493c5b217f0bf883f5
AveWycc220 <mik16748@gmail.com>
2020-12-29 13:47:52 +0300

    feat (api): add methods for login and signin

---
+++ src/api/api.js
@@ -0,0 +1,54 @@
+import Cookie from "../cookies/cookie"
+import Router from "../router/router";
+
+const cookieWorker = new Cookie()
+const router = new Router()
+
+class API {
+  constructor(url) {
+    try {
+      this.socket = new WebSocket(url)
+      this.status = 0
+    } catch(e) {
+      console.log(`Server-Error ${e}`)
+    }
+  }
+
+  signIn(form, info) {
+    let obj = {
+      command: 'user_create',
+      name: form[2].value,
+      email: form[0].value,
+      password: form[1].value
+    }
+    this.socket.send(JSON.stringify(obj))
+    this.socket.onmessage = function(e) {
+      let res = JSON.parse(e.data)
+      if (res.status === 1) {
+        router.redirectToLogin()
+      } else {
+        info.innerHTML = 'User is already created!'
+      }
+    }
+  }
+
+  logIn(form, info) {
+    let obj = {
+      command: 'user_login',
+      email: form[0].value,
+      password: form[1].value
+    }
+    this.socket.send(JSON.stringify(obj))
+    this.socket.onmessage = function(e) {
+      let res = JSON.parse(e.data)
+      if (res.status === 1) {
+        cookieWorker.add({name: 'id', value: res.user_key, maxAge: 86400})
+        router.redirectToMain()
+      } else {
+        info.innerHTML = 'Wrong Email or Password'
+      }
+    }
+  }
+}
+
+export default API
