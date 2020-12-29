92660dd203d2ece5113e2cc00f2fcecf58d466ca
AveWycc220 <mik16748@gmail.com>
2020-12-29 13:47:15 +0300

    feat (events): add class for working with events on page

---
+++ src/events/eventsHandler.js
@@ -0,0 +1,42 @@
+import DOMRender from "../render/render";
+
+const render = new DOMRender()
+
+class EventsHandler{
+  constructor({api = undefined, form = undefined, loginButton = undefined, signinButton =  undefined,
+                divInfo = undefined}) {
+    this.api = api
+    this.info = divInfo
+    this.signinEvent(form, signinButton)
+    this.loginEvent(form, loginButton)
+  }
+
+  signinEvent(form, btn) {
+    if (btn && form) {
+      btn.addEventListener('click', () => {
+        render.showInput(form[2])
+        if (form[0].value && form[1].value && form[2].value) {
+          this.api.signIn(form, this.info)
+          render.restyleForm(form, true)
+        } else {
+          render.restyleForm(form, true)
+        }
+      })
+    }
+  }
+
+  loginEvent(form, btn) {
+    if (btn && form) {
+      btn.addEventListener('click', () => {
+        if (form[0].value && form[1].value) {
+          this.api.logIn(form, this.info)
+          render.restyleForm(form)
+        } else {
+          render.restyleForm(form)
+        }
+      })
+    }
+  }
+}
+
+export default EventsHandler
