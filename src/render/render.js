3f18ce2cbced40ff4befb951873e6b5dcb8e0ee5
AveWycc220 <mik16748@gmail.com>
2020-12-29 13:46:20 +0300

    feat (render): add class render for working with DOM

---
+++ src/render/render.js
@@ -0,0 +1,28 @@
+class DOMRender {
+  showInput(input) {
+    input.style.display = 'block'
+    input.style.border = '2px solid red'
+  }
+
+  restyleForm(form, signin=false) {
+    if (!form[0].value) {
+      form[0].style.border = '2px solid red'
+    } else {
+      form[0].style.border = '2px solid black'
+    }
+    if (!form[1].value) {
+      form[1].style.border = '2px solid red'
+    } else {
+      form[1].style.border = '2px solid black'
+    }
+    if (signin) {
+      if (!form[2].value) {
+        form[2].style.border = '2px solid red'
+      } else {
+        form[2].style.border = '2px solid black'
+      }
+    }
+  }
+}
+
+export default DOMRender
