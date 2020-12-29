193a2fe204e938397d05f5343b7196ab93f1fd0b
AveWycc220 <mik16748@gmail.com>
2020-12-29 13:48:28 +0300

    feat (index): add methods for login page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import '../scss/style.scss'
 import '../assets/img/chat.ico'
+import EventsHandler from './events/eventsHandler'
+import API from './api/api'
+import Router from './router/router';
 
+const apiSocket = new API('ws://127.0.0.1:5025')
+const router = new Router();
+
+router.redirect()
+
+const eventHandler = new EventsHandler({
+  api: apiSocket,
+  form: document.querySelector('#keys'),
+  loginButton: document.querySelector('#log-in'),
+  signinButton: document.querySelector('#sign-in'),
+  divInfo: document.querySelector('.info')
+})
