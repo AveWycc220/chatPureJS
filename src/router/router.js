142b4b209307591e3bdaf583e633bd353df93f10
AveWycc220 <mik16748@gmail.com>
2021-01-14 13:42:35 +0300

    feat (router): add cookie like a arg in constructor

--- src/router/router.js
+++ src/router/router.js
@@ -1,15 +1,15 @@
-import Cookie from '../cookies/cookie';
+export default class Router {
+  constructor(cookieWorker) {
+    this.cookie = cookieWorker
+  }
 
-const cookieWorker = new Cookie()
-
-class Router {
   redirect() {
     if (process.env.MODE === 'DEV') {
-      if (cookieWorker.getCookie('id') === -1 && window.location.href !== `http://${process.env.DOMAIN}:${process.env.PORT}/login`) {
+      if (this.cookie.getCookie('id') === -1 && window.location.href !== `http://${process.env.DOMAIN}:${process.env.PORT}/login`) {
         window.location.replace(`http://${process.env.DOMAIN}:${process.env.PORT}/login`)
       }
     } else if (process.env.MODE === 'PROD') {
-      if (cookieWorker.getCookie('id') === -1 && window.location.href !== `${process.env.PROTOCOL}://${process.env.DOMAIN}/login/`) {
+      if (this.cookie.getCookie('id') === -1 && window.location.href !== `${process.env.PROTOCOL}://${process.env.DOMAIN}/login/`) {
         window.location.replace(`${process.env.PROTOCOL}://${process.env.DOMAIN}/login/`)
       }
     }
@@ -23,5 +23,3 @@ class Router {
     window.location.href = `../login`
   }
 }
-
-export default Router
