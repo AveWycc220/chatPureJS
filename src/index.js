ec0ea58000a64b98c3b00403c1ed0ecc2369ee49
AveWycc220 <mik16748@gmail.com>
2021-01-14 13:41:22 +0300

    feat (index): rework dependencies between classes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 import '../scss/style.scss'
 import '../assets/img/chat.png'
 import EventsHandler from './events/eventsHandler'
+import DOMRender from './render/render'
 import API from './api/api'
-import Router from './router/router';
+import Router from './router/router'
+import State from './state/state'
+import Cookie from './cookies/cookie'
 
-const apiSocket = new API('ws://127.0.0.1:5025')
-const router = new Router();
+const cookie = new Cookie()
+const router = new Router(cookie)
 
 router.redirect()
 
+const render = new DOMRender(cookie)
+const stateChat = new State(render)
+const apiSocket = new API('ws://127.0.0.1:5025', cookie, router, stateChat)
+
 const eventHandler = new EventsHandler({
-  api: apiSocket,
+  renderWorker: render,
+  apiWorker: apiSocket,
   form: document.querySelector('#keys'),
   loginButton: document.querySelector('#log-in'),
   signinButton: document.querySelector('#sign-in'),
-  divInfo: document.querySelector('.info')
+  divInfo: document.querySelector('.info'),
+  buttonSend: document.querySelector('#send'),
+  messageInput: document.querySelector('.textarea')
 })
+
+console.log(process.env.MODE)
