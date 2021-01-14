1c021717eaa99d3bf4b66fac31b1cc34b8352b03
AveWycc220 <mik16748@gmail.com>
2021-01-14 13:43:05 +0300

    feat (render): add renderMessage when page loaded

--- src/render/render.js
+++ src/render/render.js
@@ -1,4 +1,10 @@
-class DOMRender {
+export default class DOMRender {
+  nodeMessages = undefined
+
+  constructor(cookieWorker) {
+    this.userName = cookieWorker.getCookie('name')
+  }
+
   showInput(input) {
     input.style.display = 'block'
     input.style.border = '2px solid red'
@@ -6,6 +12,10 @@ class DOMRender {
 
   showDiv(div) {
     div.style.display = 'block'
+  }
+
+  renderName(userNameFiled) {
+    userNameFiled.innerHTML = this.userName
   }
 
   restyleForm(form, signin=false) {
@@ -27,6 +37,19 @@ class DOMRender {
       }
     }
   }
-}
 
-export default DOMRender
+  renderMessage(objMessage, node=undefined) {
+    if (node) {
+      this.nodeMessages = document.querySelector(node)
+    }
+    console.log(objMessage)
+    const message = document.createElement('div')
+    message.classList.add('message')
+    objMessage.name === this.userName ? message.classList.add('my') : message.classList.add('someone')
+    console.log(new Date(+objMessage.time))
+    message.innerHTML = `<p class="nickname">${objMessage.name}</p>
+                         <p class="time">${new Date(+objMessage.time).toString().split(' ').slice(1, 5).join(' ')}</p>
+                         <p class="message-text">${objMessage.message}</p>`
+    this.nodeMessages.append(message)
+  }
+}
