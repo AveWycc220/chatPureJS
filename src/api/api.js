16b014a77ec6e2f5c96c8b43aacf9a4ae731a06d
AveWycc220 <mik16748@gmail.com>
2021-01-14 13:46:12 +0300

    feat (api): add getMessageList method and listener for command method

--- src/api/api.js
+++ src/api/api.js
@@ -1,16 +1,14 @@
-import Cookie from "../cookies/cookie"
-import Router from "../router/router";
-
-const cookieWorker = new Cookie()
-const router = new Router()
-
-class API {
-  constructor(url) {
+export default class API {
+  constructor(url, cookieWorker, router, stateChat) {
+    this.cookie = cookieWorker
+    this.router = router
+    this.state = stateChat
     try {
       this.socket = new WebSocket(url)
-      this.socket.onmessage = function (e) {
-        console.log(JSON.parse(e.data))
-      }
+      this.socket.onopen = function () {
+        this.getMessageList()
+      }.bind(this)
+      this._listenCommand()
     } catch(e) {
       console.log(`Server-Error ${e}`)
     }
@@ -27,11 +25,11 @@ class API {
     this.socket.onmessage = function(e) {
       let res = JSON.parse(e.data)
       if (res.status === 1) {
-        router.redirectToLogin()
+        this.router.redirectToLogin()
       } else {
         info.innerHTML = 'User is already created!'
       }
-    }
+    }.bind(this)
   }
 
   logIn(form, info) {
@@ -44,13 +42,13 @@ class API {
     this.socket.onmessage = function(e) {
       let res = JSON.parse(e.data)
       if (res.status === 1) {
-        cookieWorker.add({name: 'id', value: res.user_key, maxAge: 86400, path:'/'})
-        cookieWorker.add({name: 'name', value: res.name, maxAge: 86400, path:'/'})
-        router.redirectToMain()
+        this.cookie.add({name: 'id', value: res.user_key, maxAge: 86400, path:'/'})
+        this.cookie.add({name: 'name', value: res.name, maxAge: 86400, path:'/'})
+        this.router.redirectToMain()
       } else {
         info.innerHTML = 'Wrong Email or Password'
       }
-    }
+    }.bind(this)
   }
 
   send(message) {
@@ -58,18 +56,39 @@ class API {
       command: 'message_create',
       message: message,
       time: Date.now(),
-      user_key: cookieWorker.getCookie('id')
+      user_key: this.cookie.getCookie('id')
     }
     this.socket.send(JSON.stringify(obj))
     this.socket.onmessage = function(e) {
       let res = JSON.parse(e.data)
       if (res.status === 0) {
-        cookieWorker.delete('id')
-        cookieWorker.delete('name')
-        router.redirectToLogin()
+        this.cookie.delete('id')
+        this.cookie.delete('name')
+        this.router.redirectToLogin()
       }
+    }.bind(this)
+  }
+
+  getMessageList() {
+    let obj = {
+      command: 'messages_read',
+      start: 0,
+      end: 100
+    }
+    this.socket.send(JSON.stringify(obj))
+    this.socket.onmessage = function(e) {
+      let res = JSON.parse(e.data)
+      if (res.status === 1) {
+        for (let i = 0; i < res.messages.length; i++) {
+          if (this.state.storage) { this.state.storage[i] = res.messages[i] }
+        }
+      }
+    }.bind(this)
+  }
+
+  _listenCommand() {
+    this.socket.onmessage = function (e) {
+      console.log(JSON.parse(e.data))
     }
   }
 }
-
-export default API
