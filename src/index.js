1d7134b8247a1abdaed37ed4cfbbee2be6409a41
AveWycc220 <mik16748@gmail.com>
2021-01-18 17:22:45 +0300

    feat (index): rebuild for components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import '../scss/style.scss'
 import '../assets/img/chat.png'
 import EventsHandler from './events/eventsHandler'
-import DOMRender from './render/render'
 import API from './api/api'
 import Router from './router/router'
-import State from './state/state'
 import Cookie from './cookies/cookie'
 import ComponentsHandler from './components/componentsHandler'
 import ButtonLogin from './components/loginPage/buttonLogin'
@@ -13,30 +11,27 @@ import EmailInput from './components/loginPage/emailInput'
 import PasswordInput from './components/loginPage/passwordInput'
 import UserInput from './components/loginPage/userInput'
 import RootLogin from './components/loginPage/rootLogin'
-import InfoDiv from "./components/loginPage/infoDiv";
+import InfoDiv from './components/loginPage/infoDiv'
+import NameDiv from './components/mainPage/nameDiv'
+import LogoutButton from './components/mainPage/logoutButton'
+import MessageInput from './components/mainPage/messageInput'
+import SendButton from './components/mainPage/sendButton'
+import MessageList from './components/mainPage/messageList'
+import DeleteButtonGlobal from './components/mainPage/deleteButtonGlobal'
+import EditButtonGlobal from './components/mainPage/editButtonGlobal'
+import RootMain from './components/mainPage/rootMain'
 
 const cookie = new Cookie()
 const router = new Router(cookie)
 
 router.redirect()
 
-const render = new DOMRender(cookie)
-const stateChat = new State(render)
-const eventHandler = new EventsHandler({
-  renderWorker: render,
-  apiWorker: apiSocket,
-  routerWorker: router,
-  buttonSend: document.querySelector('#send'),
-  messageInput: document.querySelector('.textarea'),
-  logoutButton: document.querySelector('#log-out'),
-})
-const apiSocket = new API(process.env.API_ADDRESS, cookie, router, stateChat, render, eventHandler)
-
+const apiSocket = new API(process.env.API_ADDRESS, cookie, router, EventsHandler)
 
 new ComponentsHandler({
-  renderWorker: render,
   apiWorker: apiSocket,
   routerWorker: router,
+  cookieWorker: cookie,
   RootLogin: RootLogin,
   InfoDiv: InfoDiv,
   ButtonLogin: ButtonLogin,
@@ -44,4 +39,12 @@ new ComponentsHandler({
   EmailInput: EmailInput,
   PasswordInput: PasswordInput,
   UserInput: UserInput,
+  NameDiv: NameDiv,
+  LogoutButton: LogoutButton,
+  MessageInput: MessageInput,
+  SendButton: SendButton,
+  MessageList: MessageList,
+  DeleteButton: DeleteButtonGlobal,
+  EditButton: EditButtonGlobal,
+  RootMain: RootMain,
 })
